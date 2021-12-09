c3a76125104eaf0dd0446115227ba1d27890ee5b
Giovanni Maggi <gvnnmaggi@gmail.com>
2021-12-09 14:03:24 +0100

    some stuff

--- src/lwc/lwcQuoteZero/lwcQuoteZero.js
+++ src/lwc/lwcQuoteZero/lwcQuoteZero.js
@@ -9,30 +9,48 @@ export default class LwcQuoteZero extends LightningElement {
     @track title = 'Initial Quote Information'
     @track isLoading = true
 
+    @track macroArgumentsSelected = []
     @track quoteInformation = [{
-        'name' : 'Name',
+        'name' : 'name',
         'apiName' : 'Name',
         'label' : undefined,
         'value' : undefined,
         'isText' : true,
+        'isLarge' : true,
+        'isVisible' : true,
     },{
         'name' : 'analysisCost',
         'apiName' : 'AnalysisCost__c',
         'label' : undefined,
         'value' : undefined,
         'isNumber' : true,
+        'isSmall' : true,
+        'isVisible' : true
     },{
         'name' : 'developCost',
         'apiName' : 'DevelopCost__c',
         'label' : undefined,
         'value' : undefined,
         'isNumber' : true,
+        'isSmall' : true,
+        'isVisible' : true
     },{
         'name' : 'testCost',
         'apiName' : 'TestCost__c',
         'label' : undefined,
         'value' : undefined,
         'isNumber' : true,
+        'isSmall' : true,
+        'isVisible' : true
+    },{
+        'name' : 'macroArguments',
+        'apiName' : 'MacroArguments__c',
+        'label' : undefined,
+        'value' : undefined,
+        'isCombobox' : true,
+        'options' : [],
+        'isNumber' : true,
+        'isVisible' : true
     }]
 
     connectedCallback() {
@@ -40,6 +58,7 @@ export default class LwcQuoteZero extends LightningElement {
         console.log('hey fromzero!', JSON.parse(JSON.stringify(this.macroArguments)))
         for(let element of this.quoteInformation) {
             element.label = this.labelFields[element.apiName]
+            if(element.apiName === 'MacroArguments__c') element.options = this.macroArguments
         }
         console.log('hey fromzero!', JSON.parse(JSON.stringify(this.quoteInformation)))
         this.isLoading = false
